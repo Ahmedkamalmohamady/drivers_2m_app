75522ab6e34c1ad3bd49d762e6964942d45d6a80
Ahmedkamalmohamady <Ahmedkamalmohamady@gmail.com>
2023-08-08 10:04:15 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:drivers_2m_app/splashScreen/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 import 'package:provider/provider.dart';
 import 'infoHandler/app_info.dart';
 
 void main() async
 {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     MyApp(
